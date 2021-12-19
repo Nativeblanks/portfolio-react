6bb37b3a6eb87cde1f67dadeb71fc6f112fccdd2
Zach B <nativeblanks@gmail.com>
2021-12-19 18:12:45 -0500

    resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
       <section className="about-section">
           <h1 id="about">Who am I? Zach Blanks</h1>
+          <a href="https://my.indeed.com/p/zacharyb-9q5mp52/pdf">Click to download my resume</a>
       </section>
     )
 }
