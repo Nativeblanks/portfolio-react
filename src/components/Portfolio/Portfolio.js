4c8d31d604a2f0b1b3d621e52e21015059bf3a7a
Zach B <nativeblanks@gmail.com>
2021-12-19 18:05:53 -0500

    imgs

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,43 @@
 import React from "react";
 
+import TechBlog from "../Images/tech-blog.png"
+import PasswordGen from "../Images/password-gen.png"
+import NoteTaker from "../Images/NoteTaker.gif"
+import jsquiz from "../Images/jsquiz.png"
+import pizzaMaker from "../Images/pizza-maker.gif"
+import Budget from "../Images/Budet-Tracker.gif"
+
+
+
 const Portfolio = () => (
     <div>
       <h1>Portfolio Page</h1>
       <p>
       Tech blog
+      <img 
+        className="d-block w-100"
+        src= {TechBlog} />
+
       Password Generator
+      <img 
+        className="d-block w-100"
+        src= {PasswordGen} />
       Note Taker
+      <img 
+        className="d-block w-100"
+        src= {NoteTaker} />
       jsquiz
+      <img 
+        className="d-block w-100"
+        src= {jsquiz} />
       pizza maker
+      <img 
+        className="d-block w-100"
+        src= {pizzaMaker} />
       budget tracker
+      <img 
+        className="d-block w-100"
+        src= {Budget} />
       </p>
     </div>
   );
