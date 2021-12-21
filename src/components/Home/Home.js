f797a3e8fe1f0c4a8ce6df0b8f953bada356c566
Zach B <nativeblanks@gmail.com>
2021-12-21 14:16:30 -0500

    predeployment

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,36 +26,36 @@ export default function Home() {
           />
           <Carousel.Caption>
             <h3>The Note Taker app</h3>
-            <p>Tech used</p>
+            <p>Accessing and updating database functionality</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="d-block w-100 imageTest"
+            className="d-block w-100"
             src={TechBlog}
             alt="Tech Blog"
           />
           <Carousel.Caption>
             <h3>Tech Blog</h3>
-            <p>Tech used</p>
+            <p>Database with handlebars and express</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="d-block w-100 imageTest"
+            className="d-block w-100"
             src={PasswordGen}
-            alt="Tech Blog"
+            alt="Password Gen"
           />
           <Carousel.Caption>
-            <h3>Label for second slide</h3>
-            <p>Tech used</p>
+            <h3>Password Generator</h3>
+            <p>JavaScript powered random passwords</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="d-block w-100 imageTest"
+            className="d-block w-100"
             src={pizzaMaker}
-            alt="Tech Blog"
+            alt="Pizza Time"
           />
           <Carousel.Caption>
             <h3>Build your own pizza</h3>
@@ -64,9 +64,9 @@ export default function Home() {
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="d-block w-100 imageTest"
+            className="d-block w-100"
             src={jsquiz}
-            alt="Tech Blog"
+            alt="JavaScript Quiz"
           />
           <Carousel.Caption>
             <h3>JavaScript quiz</h3>
@@ -75,9 +75,9 @@ export default function Home() {
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="d-block w-100 imageTest"
+            className="d-block w-100"
             src={Budget}
-            alt="Tech Blog"
+            alt="Buget tracker"
           />
           <Carousel.Caption>
             <h3>Budget tracker</h3>
